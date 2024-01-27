e5fe0a9a258bedea81b271bd7a6d902df9d035e9
ALiSAKUNA <alinikooobidel@gmail.com>
2024-01-27 15:53:36 +0330

    Ido My Tasks(Create ProductCategory Model)

--- product/models.py
+++ product/models.py
@@ -8,3 +8,8 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+    
+class ProductCategory(models.Model):
+    title =models.CharField(max_length=300)
+    is_active =models.BooleanField(null=True)
+    is_delete =models.BooleanField(default=False)
