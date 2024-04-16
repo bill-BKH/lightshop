22ba0dfb328488aad057b0d5a85c4d1a6d98cfea
aminhesami-1 <amin12hesami@gmail.com>
2024-04-16 16:04:07 +0330

    added user panel carts and cart details urls

--- user_panel/urls.py
+++ user_panel/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-
+app_name = 'user_panel'
 urlpatterns = [
     path('',views.user_profile),
     path('carts',views.carts),
-    path('details<int:id>',views.detail , name="detail"),
+    path('details/<int:id>',views.detail , name="detail"),
 ]
