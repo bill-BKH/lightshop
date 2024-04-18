4ec1cca3d2548cc9e56b7d3e541a9727e3905654
ALiSAKUNA <alinikooobidel@gmail.com>
2024-04-18 09:36:54 +0330

    Add A Section About AddAddressModel to Admin Page

--- user_panel/admin.py
+++ user_panel/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import AddAddress
 # Register your models here.
+class ContactUsAdmin(admin.ModelAdmin):
+    list_filter = ['read_by_admin']
+admin.site.register(AddAddress)
