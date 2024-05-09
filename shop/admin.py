d218d25cdfdcb2aed6f4b1a5fd96e9d6eae352b1
ALiSAKUNA <alinikooobidel@gmail.com>
2024-05-09 09:43:09 +0330

    slider model added the shop/admin.py

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import slider
+# Register your models here.
+class SliderAdmin(admin.ModelAdmin):
+    admin.site.register(slider)
 # Register your models here.
