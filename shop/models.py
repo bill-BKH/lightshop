f1d512ba8c5ac827040ebe614842e43b59dfc942
ALiSAKUNA <alinikooobidel@gmail.com>
2024-05-09 09:41:52 +0330

    i created a slider model in shop/models.py

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class slider(models.Model):
+    first_slider=models.ImageField(null=True,blank=True)
+    second_slider=models.ImageField(null=True,blank=True)
+    third_slider=models.ImageField(null=True,blank=True)
+    fourth_slider=models.ImageField(null=True,blank=True)
+    fifth_slider=models.ImageField(null=True,blank=True)
+    sixth_slider=models.ImageField(null=True,blank=True)
+    seventh_slider=models.ImageField(null=True,blank=True)
+    sidebar=models.ImageField(null=True,blank=True)
