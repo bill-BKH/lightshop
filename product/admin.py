96783c6fb8c536d7be35a3569c3d6575fe8f87ce
ALiSAKUNA <alinikooobidel@gmail.com>
2024-05-09 10:22:27 +0330

    i registered "Brand" model

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product , ProductCategory,ProductComment,ProductVisit
+from .models import Product , ProductCategory,ProductComment,ProductVisit,Brand
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(ProductCategory)
+admin.site.register(Brand)
 admin.site.register(ProductComment)
 admin.site.register(ProductVisit)
