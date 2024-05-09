4a649371d5efc858a2849bf7c09098aec1fdc5d7
ALiSAKUNA <alinikooobidel@gmail.com>
2024-05-09 09:45:38 +0330

    in slider_pack i geted slider model that id=1 and render that

--- shop/views.py
+++ shop/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from product.models import ProductCategory, Product
 from product.models import ProductVisit
 from utils.http_service import get_client_ip
+from .models import slider
 # Create your views here.
 def index(request):
     user_ip = get_client_ip(request)
@@ -10,9 +11,9 @@ def index(request):
     visited_product = [] 
     for visit in visited_product2:
         visited_product.append(Product.objects.get(id =visit.product.id))
-
-
-    return render(request, 'shop/index.html',{'visited_product':visited_product})
+    slider_pack=slider.objects.get(id=1)
+    
+    return render(request, 'shop/index.html',{'visited_product':visited_product,'slider_pack':slider_pack})
 
 
 def catogory_shard(request):
