ba1fd810906d9e53d75f0ef11d5360afba454751
aminhesami-1 <amin12hesami@gmail.com>
2024-05-10 21:35:35 +0330

    added new blog app that funtional and have all blog and single blog page and have comment model that relates to a blog have like and dislike fields that not functional

---
+++ blog/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from account.models import User
+# Create your models here.
+class blog(models.Model):
+    user = models.ForeignKey(User ,  on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    created_date = models.DateField(auto_now=True)
+    image = models.ImageField(upload_to="blog")
+    main_text = models.CharField(max_length=1024)
+    slug = models.SlugField()
+
+class blog_comment(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    blog = models.ForeignKey(blog , on_delete=models.CASCADE)
+    created_date = models.DateField(auto_now=True)
+    text = models.CharField(max_length=512)
+    admin_respond = models.CharField(max_length=512, blank=True)
+    admin_vrify = models.BooleanField(default=False)
+    like = models.IntegerField(default=0)
+    dislike = models.IntegerField(default=0)
+    user_liked = models.ManyToManyField(User ,blank=True, related_name='blog_liked')
+    user_dislike = models.ManyToManyField(User ,blank=True, related_name="blog_disliked")
+
+    def has_user_liked(self, user):
+        return user in self.user_liked.all()
+    def has_user_disliked(self , user):
+        return user in self.user_dislike.all()
---
+++ blog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from.models import blog , blog_comment
+# Create your views here.
+def blogs(request):
+    blogs = blog.objects.all()
+    
+    return render(request , 'blog/blog.html' , {"blogs" : blogs})
+
+def blog_detail(request , slug) :
+    single_blog = blog.objects.get(slug=slug)
+    comments = blog_comment.objects.filter(blog=single_blog , admin_vrify=True)
+    print('-'*88)
+    print(single_blog)
+    print(comments)
+    print('-'*88)
+    return render(request , 'blog/single-blog.html', {'blog' : single_blog , 'commnets' : comments})
