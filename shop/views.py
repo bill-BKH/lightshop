b62c3c01719a56374030785bca7d91ea793e73e4
ALiSAKUNA <alinikooobidel@gmail.com>
2024-05-16 11:33:22 +0330

    i created a MainSlaider model and added to admin page and get that in views.py

--- shop/views.py
+++ shop/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from product.models import ProductCategory, Product
 from product.models import ProductVisit
 from utils.http_service import get_client_ip
+from .models import MainSlider
 # Create your views here.
 def index(request):
     user_ip = get_client_ip(request)
@@ -11,9 +12,10 @@ def index(request):
     for visit in visited_product2:
         visited_product.append(Product.objects.get(id =visit.product.id))
     most_solds = Product.objects.order_by('-sold')[:10]
+    mainslider=MainSlider.objects.all()
 
 
-    return render(request, 'shop/index.html',{'visited_product':visited_product, "most_solds":most_solds})
+    return render(request, 'shop/index.html',{'visited_product':visited_product, "most_solds":most_solds,'mainslider':mainslider})
 
 
 def catogory_shard(request):
