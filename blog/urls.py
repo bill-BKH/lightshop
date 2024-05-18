d97bd8a331390ae94ec9e33964b8312d29ceb91b
aminhesami-1 <amin12hesami@gmail.com>
2024-05-18 16:12:02 +0330

    like and dislike fetch urls added

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ app_name='blog'
 urlpatterns = [
     path('', views.blogs , name='blog'),
     path('<slug:slug>', views.blog_detail , name='blog-detail'),
+    path('like/', views.blog_comment_like , name='like'),
+    path('dislike/', views.blog_comment_dislike , name='dislike'),
 
 ]
